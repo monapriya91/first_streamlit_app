9c639653577792db9e6013bbce3029b5f71e45ea
Mohanapriya R <118175302+monapriya91@users.noreply.github.com>
2022-11-14 16:22:16 +0530

    Update streamlit_app.py
    added favs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry')
 streamlit.text('Kale, Spinach,Smoothie')
 streamlit.text('Boiled egg')
+streamlit.text('Avacado')
